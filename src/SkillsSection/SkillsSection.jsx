494b5eff82db19ffd72445e79b2b6e5bf4423b43
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-11-03 10:51:06 +0530

    Minor change in Layout for skills section

--- src/SkillsSection/SkillsSection.jsx
+++ src/SkillsSection/SkillsSection.jsx
@@ -4,8 +4,10 @@ import { Box, Grid2 } from '@mui/material';
 
 const SkillsSection = () => {
   return (
-    <Grid2 container spacing={0} border={1} p={2} alignItems={"center"} flexDirection={"column"}>
-      <h1>Skills</h1>
+    <Grid2 container spacing={0} border={1} p={2} flexDirection={"column"}>
+      <Grid2 container justifyContent={{xs: "center", sm:"left"}}>
+        <h1>Skills</h1>
+      </Grid2>
       <SkillsList skills={skills}/>
     </Grid2>
   )
