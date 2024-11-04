675459203599603d48aa72bf65adc94bc5df146b
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-11-05 00:33:18 +0530

    Changing top padding of the about section

--- src/Sections/AboutSection/AboutSection.jsx
+++ src/Sections/AboutSection/AboutSection.jsx
@@ -5,9 +5,9 @@ import data from '../../Data/about.json'
 
 const AboutSection = () => {
   return (
-      <Grid2 id='about'container direction={"column"} justifyItems={"center"} pt={"3rem"}>
+      <Grid2 id='about' container direction={"column"} justifyItems={"center"} pt={1}>
         <Box mb={1.5}>
-          <Box component={"h1"} my={1}>{data.heading}</Box>
+          <Box component={"h1"} mb={1} pt={0}>{data.heading}</Box>
           <Box component={"q"}>{data.quote}</Box>
         </Box>
         <AboutMe2 aboutMeData={data}/>
