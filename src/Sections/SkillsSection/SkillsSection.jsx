5cd60b0fcaae4ab37a73fa79cd4fd90974c416cc
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-11-06 00:05:43 +0530

    Removing section div in skills section

--- src/Sections/SkillsSection/SkillsSection.jsx
+++ src/Sections/SkillsSection/SkillsSection.jsx
@@ -5,14 +5,12 @@ import { Box, Grid2 } from '@mui/material';
 
 const SkillsSection = () => {
   return (
-    <section >
       <Grid2 id='skills' container pt={"3rem"} spacing={0} flexDirection={"column"}>
         <Grid2 container justifyContent={{xs: "center", sm:"left"}}>
           <h1>Skills</h1>
         </Grid2>
         <SkillsList2 skills={skills}/>
       </Grid2>
-    </section>
   )
 }
 
