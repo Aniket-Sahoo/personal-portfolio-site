99454b72603b4406cede285e82fe0ac05ed19ab2
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-11-06 17:08:46 +0530

    Changed spacing in the emoji

--- src/HeroComponent/HeroComponent.jsx
+++ src/HeroComponent/HeroComponent.jsx
@@ -9,14 +9,14 @@ const HeroText = () => {
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
   const content = isSmallScreen? heroText.small : heroText.normal;
   return (
-    <Container px={{xs: "1rem", sm: "2rem", md: "4.5rem", lg: "6rem", xl: "10rem", xxl: "12rem"}}>
+    <Container sx={{px:{xs: "1rem", sm: "2rem", md: "4.5rem", lg: "6rem", xl: "9rem", xxl: "12rem"}}}>
       <Box>
         <Typography variant="h6" pb={0.5}>
           {content.greeting}
         </Typography>
         <Typography 
           variant="h1"
-          fontSize={{xs: "3rem", sm: "3rem", md: "3.5rem", lg: "4rem"}}
+          fontSize={{xs: "3rem", sm: "3.5rem", md: "4rem", lg: "5rem"}}
           sx={{
             background: theme.palette.gradientBackground.primary,
             WebkitBackgroundClip: 'text',
@@ -27,19 +27,19 @@ const HeroText = () => {
         </Typography>
       </Box>
       <Box py={3} pb={6}>
-        <Typography variant="subtitle1" color='text.secondary' fontSize={"1.3rem"}>  
+        <Typography variant="subtitle1" color='text.secondary' fontSize={{xs:"1.15rem", sm:"1.3rem"}}>  
           <Box component={"span"} sx={{fontWeight: "bold", color: "text.primary"}}>
             {content.about.role}
           </Box>
             {content.about.tagline}
         </Typography>
       </Box>
-      <Box color={"text.secondary"}>
+      <Box color={"text.secondary"} pl={0}>
         <Typography variant="subtitle1">  
-          {content.anecdote1.emoji} {content.anecdote1.text}
+          {content.anecdote1.emoji}&nbsp; {content.anecdote1.text}
         </Typography>
         <Typography variant="subtitle1">  
-          {content.anecdote2.emoji} {content.anecdote2.text}
+          {content.anecdote2.emoji}&nbsp; {content.anecdote2.text}
         </Typography>
       </Box>
     </Container>
