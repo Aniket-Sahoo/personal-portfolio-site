bce3bfb6d0b2877ebc8fe24cc5b8a3162a7fabca
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-11-06 21:38:56 +0530

    Changed Projects heading to My Projects

--- src/Sections/ProjectsSection/ProjectsSection.jsx
+++ src/Sections/ProjectsSection/ProjectsSection.jsx
@@ -5,7 +5,7 @@ const ProjectsSection = () => {
   return (
     <Grid2 id='projects' container flexDirection={"column"} pt={"3rem"} alignContent={"center"} >
       <Grid2 container justifyContent={{xs: "center", sm:"left"}} spacing={3}>
-        <Typography variant="h1">Projects</Typography>
+        <Typography variant="h1">My Projects</Typography>
       </Grid2>
         <ProjectCardList />
     </Grid2>
